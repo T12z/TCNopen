fe01d366116bb3443615ed659cb3031f84db9072
bloehr <bloehr@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2019-06-17 15:30:18 +0000

    initial
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@1915 3b5a3598-5f4e-4449-9e63-bd40438bfec0

---
+++ trdp/src/api/trdp_if_soa_.h
@@ -0,0 +1,40 @@
+/**********************************************************************************************************************/
+/**
+ * @file            trdp_if_soa.h
+ *
+ * @brief           Service oriented interface
+ *
+ * @details         This header file defines the proposed extensions and additions to IEC61375-2-3:2017 regarding
+ *                  the service oriented interface
+ *
+ * @note            Project: TCNOpen TRDP prototype stack & FDF/DbD
+ *
+ * @author          Bernd Loehr, NewTec GmbH, 2019-06-17
+ *
+ * @remarks         Copyright 2019, NewTec GmbH
+ *
+ *
+ * $Id$
+ *
+ */
+
+#ifndef TRDP_IF_SOA_H
+#define TRDP_IF_SOA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include    "iec61375-2-3.h"
+
+/***********************************************************************************************************************
+ * DEFINITIONS
+ */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
