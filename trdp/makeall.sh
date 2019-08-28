8a0eb55533e44622a1dbd5e7317477d8af461a4a
thorsten@ChickenJoe <thorsten.schulz@uni-rostock.de>
2019-08-28 18:59:46 +0200

    add a personal skript to bulk-build libraries for multiple architectures

---
+++ trdp/makeall.sh
@@ -0,0 +1,16 @@
+make LINUX_armhf_config
+make clean DEBUG=TRUE libtrdp
+make libtrdp
+
+make ELINOS_x86_64_config
+make DEBUG=TRUE libtrdp
+make libtrdp
+
+make LINUX_X86_config
+make DEBUG=TRUE libtrdp
+make libtrdp
+
+make LINUX_X86_64_config
+make DEBUG=TRUE libtrdp
+make libtrdp
+
