3f84a36c801c208658e8cc7aecbdd3a6d54ecbf6
andersoberg <andersoberg@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2020-05-04 07:35:22 +0000

    #330 Extend TRDP_PDTest with TSN support
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@2164 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/test/pdpatterns/trdp-pd-test.c
+++ trdp/test/pdpatterns/trdp-pd-test.c
@@ -20,6 +20,7 @@
  *      BL 2018-06-20: Ticket #184: Building with VS 2015: WIN64 and Windows threads (SOCKET instead of INT32)
  *      BL 2018-03-06: Ticket #101 Optional callback function on PD send
  *      BL 2017-06-30: Compiler warnings, local prototypes added
+ *      AÖ 2020-05-04: Ticket #330: Extend TRDP_PDTest with TSN support
  *
  */
 
