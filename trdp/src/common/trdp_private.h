a32a77c5cd992b86d124f6e4ea68c8f99b1f1fca
bloehr <bloehr@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2020-08-21 15:50:11 +0000

    Back to trunk version
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@2217 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/src/common/trdp_private.h
+++ trdp/src/common/trdp_private.h
@@ -65,9 +65,9 @@
 /* The TRDP version can be predefined as CFLAG   */
 #ifndef TRDP_VERSION
 #define TRDP_VERSION    2
-#define TRDP_RELEASE    0
-#define TRDP_UPDATE     3
-#define TRDP_EVOLUTION  0              /* Evolution > 0 denotes trunk! */
+#define TRDP_RELEASE    1
+#define TRDP_UPDATE     0
+#define TRDP_EVOLUTION  1              /* Evolution > 0 denotes trunk! */
 #endif
 
 /* Version as a string, this can also be for example 1.2.3.4.RC1 */
