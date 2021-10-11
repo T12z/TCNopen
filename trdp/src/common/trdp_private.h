b5b658db1adac7f7092a34e9d377105ce7b5ef85
ahweiss <ahweiss@3b5a3598-5f4e-4449-9e63-bd40438bfec0>
2021-10-11 11:42:29 +0000

    ver 2.2.23.17
    git-svn-id: https://svn.code.sf.net/p/tcnopen/trdp/trunk@2317 3b5a3598-5f4e-4449-9e63-bd40438bfec0

--- trdp/src/common/trdp_private.h
+++ trdp/src/common/trdp_private.h
@@ -67,7 +67,7 @@
 #define TRDP_VERSION    2
 #define TRDP_RELEASE    2
 #define TRDP_UPDATE     23
-#define TRDP_EVOLUTION  13              /* Evolution > 0 denotes trunk! */
+#define TRDP_EVOLUTION  17              /* Evolution > 0 denotes trunk! */
 #endif
 
 /* Version as a string, this can also be for example 1.2.3.4.RC1 */
